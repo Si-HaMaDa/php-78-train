f1b482dbdd6ae00eea547af6cb4f84e0ee63c3be
Si HaMaDa <nrookman_27@yahoo.com>
2022-11-17 14:22:09 +0200

    Main admin layout

---
+++ admin/parts/sideNav.php
@@ -0,0 +1,67 @@
+<div class="position-sticky pt-3 sidebar-sticky">
+    <ul class="nav flex-column">
+        <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="/admin">
+                <span data-feather="home" class="align-text-bottom"></span>
+                Dashboard
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="./tags.php">
+                <span data-feather="file" class="align-text-bottom"></span>
+                Tags
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="./categories">
+                <span data-feather="shopping-cart" class="align-text-bottom"></span>
+                Categories
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="./posts.php">
+                <span data-feather="layers" class="align-text-bottom"></span>
+                Posts
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="./users.php">
+                <span data-feather="users" class="align-text-bottom"></span>
+                Users
+            </a>
+        </li>
+    </ul>
+
+    <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted text-uppercase">
+        <span>Saved reports</span>
+        <a class="link-secondary" href="#" aria-label="Add a new report">
+            <span data-feather="plus-circle" class="align-text-bottom"></span>
+        </a>
+    </h6>
+    <ul class="nav flex-column mb-2">
+        <li class="nav-item">
+            <a class="nav-link" href="#">
+                <span data-feather="file-text" class="align-text-bottom"></span>
+                Current month
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">
+                <span data-feather="file-text" class="align-text-bottom"></span>
+                Last quarter
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">
+                <span data-feather="file-text" class="align-text-bottom"></span>
+                Social engagement
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">
+                <span data-feather="file-text" class="align-text-bottom"></span>
+                Year-end sale
+            </a>
+        </li>
+    </ul>
+</div>
