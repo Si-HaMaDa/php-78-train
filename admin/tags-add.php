59fb4dbc7194836d1d09c9a70cb2ba4273c7b655
Si HaMaDa <nrookman_27@yahoo.com>
2022-11-17 16:06:00 +0200

    Add Tag Page

---
+++ admin/tags-add.php
@@ -0,0 +1,47 @@
+<?php
+require 'parts/header.php';
+$errors = [];
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $name = $_POST['name'];
+
+    if (empty($name)) {
+        $errors['name'] = "Name is empty";
+    }
+
+    if (empty($errors)) {
+        try {
+            $sql = "INSERT INTO tags (name) VALUES ('$name')";
+            $conn->exec($sql);
+            $_SESSION['success'] = 'Tag added Successfuly';
+            // header('location: ./tags.php');
+            echo '<script>window.location = "tags.php"</script>';
+            die;
+        } catch (PDOException $e) {
+            $errors[] = "<h1>Error: " . $e->getMessage() . "</h1>";
+        }
+    }
+}
+
+?>
+
+<?php include 'parts/validation-errors.php'; ?>
+
+<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+
+    <h1 class="h2">Add Tag</h1>
+    <a href="tags.php" class="btn btn-warning">
+        < back</a>
+</div>
+<div class="container card my-3 p-3">
+    <form method="POST">
+        <div class="form-floating my-3">
+            <input type="text" name="name" class="form-control" id="name" placeholder="Name">
+            <label for="name">Name</label>
+        </div>
+        <button class="w-100 btn btn-lg btn-primary" type="submit">Save</button>
+    </form>
+</div>
+
+<?php
+require 'parts/footer.php';
--- admin/tags.php
+++ admin/tags.php
@@ -14,9 +14,9 @@ try {
 <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
     <h1 class="h2">Tag</h1>
     <div class="btn-toolbar mb-2 mb-md-0">
-        <button type="button" class="btn btn-sm btn-outline-secondary">
+        <a href="./tags-add.php" type="button" class="btn btn-sm btn-outline-secondary">
             Add Tag
-        </button>
+        </a>
     </div>
 </div>
 
